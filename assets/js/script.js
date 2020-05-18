6a864f655b1d7b141aab2b98c9446e039716e0d7
Daniel Fliguel Guz <daniel.fliguel@tbfconsultoria.com.br>
2020-05-18 01:46:14 -0300

    ~Javascript, rodapé e ajustes

---
+++ assets/js/script.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+
+    $(".botaoRoxo").click(function(){
+        $("body").css("background-color","purple");
+    });
+
+    $(".botaoVermelho").click(function(){
+        $("body").css("background-color","red");
+    });
+
+    $(".botaoLaranja").click(function(){
+        $("body").css("background-color","orange");
+    });
+
+    $(".botaoAmarelo").click(function(){
+        $("body").css("background-color","yellow");
+    });
+
+    $(".botaoCinza").click(function(){
+        $("body").css("background-color","grey");
+    });
+
+    $(".botaoPadrao").click(function(){
+        $("body").css("background-color","#ABEC54");
+    });
+
+})
